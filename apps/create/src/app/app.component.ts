a15074467d94a7434cb73c30404a5ad406e02006
Fabian Birke <fabian.birke@t-online.de>
2023-05-21 20:46:54 +0200

    Initial commit

---
+++ apps/create/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'create';
+}
